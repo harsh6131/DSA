634ecc1dd7a167012a73f935ee069ec49e1fa1e9
Harshvardhan Singh <harshvardhan.24bce11064@vitbhopal.ac.in>
2026-08-26 21:51:32 +0530

    Time: 1 ms (90.99%) | Memory: 43.5 MB (54.77%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int titleToNumber(String col) {
+        
+        int sum=0, p=0;
+        int n = col.length();
+
+        for(int i=n-1; i>=0; i--){
+
+            int tmp = col.charAt(i)-64;
+            sum = sum+(int)Math.pow(26,p++)*tmp;
+
+        }
+        return sum;
+    }
+}
