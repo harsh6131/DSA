089660218b2baee60d19b7d3b2f3b614bf72ebea
Harshvardhan Singh <harshvardhan.24bce11064@vitbhopal.ac.in>
2026-09-01 23:09:25 +0530

    Time: 4 ms (36.47%) | Memory: 43.1 MB (72.97%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char i : s.toCharArray()){
+            if(isBracketOpen(i)){
+                stack.push(i);
+            }else{
+                if(stack.size() == 0){
+                    return false;
+                }else{
+                    if(areBracketsMatching(stack.peek(),i)){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.size() == 0;
+        
+    }
+    private boolean isBracketOpen(char c){
+        switch (c) {
+            case '(':
+            case '[':
+            case '{':
+                return true;
+            default:
+                return false;
+        }
+    }
+    private boolean areBracketsMatching(char c, char d) {
+        if (c == '(' && d == ')') {
+            return true;
+        } else if (c == '{' && d == '}') {
+            return true;
+        } else if (c == '[' && d == ']') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
